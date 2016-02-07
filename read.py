6b7e28d57cf6e6e1921c2ea1096f616cecfada49
YasaswiK <yasaswik.bcl2@gmail.com>
2016-02-07 10:57:55 +0530

    Added functions for entropy, info gain and choosing best attribute

--- read.py
+++ read.py
@@ -15,25 +15,52 @@ with open('dataset_v2.csv') as f:
 		category.append(row[1])
 		i+=1
 
-def build_occurrence_list():
-	for i in range(len(tweets)):
-		words = nltk.word_tokenize(tweets[i])
-		for blub in words:
-			if (category[i]=='1'):
-				if (occurence_list_1.__contains__(blub)):
-					occurence_list_1[blub]+=1
-				else :
-					occurence_list_1[blub]=1
-			if (category[i]=='2'):
-				if (occurence_list_2.__contains__(blub)):
-					occurence_list_2[blub]+=1
-				else :
-					occurence_list_2[blub]=1
-			if (category[i]=='3'):
-				if (occurence_list_3.__contains__(blub)):
-					occurence_list_3[blub]+=1
-				else :
-					occurence_list_3[blub]=1
-	return 
+# function to calculate entropy 
+# parameters - dataset, target_attribute
+def calc_entropy(data_set, target_attribute):
+	val_freq     = {}
+	data_entropy = 0.0
+
+	# Calculate the frequency of each of the values in the target attr
+	for record in data:
+		if (val_freq.has_key(record[target_attr])):
+			val_freq[record[target_attr]] += 1.0
+		else:
+			val_freq[record[target_attr]]  = 1.0
+	#Calculate the entropy of the data for the target attribute
+	for freq in val_freq.values():
+		data_entropy += (-freq/len(data)) * math.log(freq/len(data), 2) 
+	return data_entropy
+
+def gain(data, attr, target_attr):
+	"""
+	Calculates the information gain (reduction in entropy) that would
+	result by splitting the data on the chosen attribute (attr).
+	"""
+	val_freq       = {}
+	subset_entropy = 0.0
+
+	# Calculate the frequency of each of the values in the target attribute
+	for record in data:
+		if (val_freq.has_key(record[attr])):
+		    val_freq[record[attr]] += 1.0
+		else:
+		    val_freq[record[attr]]  = 1.0
+
+	# Calculate the sum of the entropy for each subset of records weighted
+	# by their probability of occuring in the training set.
+	for val in val_freq.keys():
+		val_prob        = val_freq[val] / sum(val_freq.values())
+		data_subset     = [record for record in data if record[attr] == val]
+		subset_entropy += val_prob * entropy(data_subset, target_attr)
+
+	# Subtract the entropy of the chosen attribute from the entropy of the
+	# whole data set with respect to the target attribute (and return it)
+	return (entropy(data, target_attr) - subset_entropy)		
+
+def best_attribute(data, attributes, target_attribute):
+	list_gain = []
+	for attr in attributes:
+		list_gain.append(data, attr, target_attribute)
+	return attributes[list_gain.index(max(list_gain))]
 		
-build_occurrence_list()

e3452e06c9998761023c6ecb4109a2dc9d9a45f0
YasaswiK <yasaswik.bcl2@gmail.com>
2016-02-07 10:27:23 +0530

    Modified readme, screwed most of the existing code. need to do it properly again

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # decision-tree-tweets
 An implementation of decision trees for a given dataset of tweets. 
+#TODO
+1. A function to split the tweets based on an attribute (one list with the tweets that have the word, one list with the tweets that don't) (returns a tuple)
+2. A function that chooses the best attribute based on entropy and information gain
+
+#Algorithm
+tree = dictionary{}
+function build-tree(data-set, attribute-set)
+	if (category of all elements in the data-set is the same)
+		do nothing, return
+	else
+		attribute = choose-next-best(data-set, attribute-set)
+		split-tuple = split-dataset(data-set, attribute)
+		attribute-set.remove(attribute)
+		tree = {attribute:{}}
+		// for the left child, pass the dataset which have the word
+		subtree-1 = build-tree(split-tuple[0], attribute-set)
+		// for the right child, pass the dataset that doesn't have the word
+		subtree-2 = build-tree(split-tuple[1], attribute-set)
+		tree[attribute][true] = subtree-1	
+		tree[attribute][false] = subtree-2
+		return tree
--- read.py
+++ read.py
@@ -6,9 +6,6 @@ category = []
 occurence_list_1= dict() 
 occurence_list_2= dict() 
 occurence_list_3= dict() 
-reverse_dict_1 = dict()
-reverse_dict_2 = dict()
-reverse_dict_3 = dict()
 
 with open('dataset_v2.csv') as f:
 	read = csv.reader(f)
@@ -21,76 +18,22 @@ with open('dataset_v2.csv') as f:
 def build_occurrence_list():
 	for i in range(len(tweets)):
 		words = nltk.word_tokenize(tweets[i])
-		#print words
-		#print category[i]
 		for blub in words:
-			#print "category is ", category[i]
 			if (category[i]=='1'):
-				#print "In one"
 				if (occurence_list_1.__contains__(blub)):
 					occurence_list_1[blub]+=1
 				else :
 					occurence_list_1[blub]=1
-
-
 			if (category[i]=='2'):
-				#print "In two"
 				if (occurence_list_2.__contains__(blub)):
 					occurence_list_2[blub]+=1
 				else :
 					occurence_list_2[blub]=1
 			if (category[i]=='3'):
-				#print "In three"
 				if (occurence_list_3.__contains__(blub)):
 					occurence_list_3[blub]+=1
 				else :
 					occurence_list_3[blub]=1
-	#print occurence_list_1
-	#print occurence_list_2
-	#print occurence_list_3
-	for blub in occurence_list_1.keys():
-		# reverse dictionary
-		if (reverse_dict_1.__contains__(occurence_list_1[blub])):
-			reverse_dict_1[occurence_list_1[blub]].append(blub)
-		else:
-			reverse_dict_1[occurence_list_1[blub]] = [blub]
-	for blub in occurence_list_2.keys():
-		# reverse dictionary
-		if (reverse_dict_2.__contains__(occurence_list_2[blub])):
-			reverse_dict_2[occurence_list_2[blub]].append(blub)
-		else:
-			reverse_dict_2[occurence_list_2[blub]] = [blub]
-	for blub in occurence_list_3.keys():
-		# reverse dictionary
-		if (reverse_dict_3.__contains__(occurence_list_3[blub])):
-			reverse_dict_3[occurence_list_3[blub]].append(blub)
-		else:
-			reverse_dict_3[occurence_list_3[blub]] = [blub]
-
-	count = 0
-	print "Dictionary one"
-	for i in sorted(reverse_dict_1, reverse=True):
-		print i, reverse_dict_1[i]
-		#if (count==5):
-		#	break
-		count+=1
-	
-	count = 0
-	print "Dictionary two "
-	for i in sorted(reverse_dict_2, reverse=True):
-		print i, reverse_dict_2[i]
-		#if (count==5):
-		#	break
-		count+=1
-	count = 0
-	print "Dictionary three"
-	for i in sorted(reverse_dict_3, reverse=True):
-		print i, reverse_dict_3[i]
-		#if (count==5):
-		#	break
-		count+=1
 	return 
 		
 build_occurrence_list()
-#print tweets
-#print category
